1497aa9afeca2e4e69e45ec85808c847b114474a
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-03-22 20:50:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,10 +21,17 @@ class Student
     SQL
 
     student = DB[:conn].execute(sql, name)[0]
-  #  binding.pry
     self.new_from_db(student)
   end
 
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students WHERE GRADE = 9
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+  
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
