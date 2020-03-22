4ea3d866e962ff92b4db7bdaf6819567eccc9f13
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-03-22 19:32:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,14 +1,12 @@
 class Student
   attr_accessor :id, :name, :grade
 
-  def initialize(name, grade, id=nil)
-    @name = name
-    @grade = grade
-    @id = id
-  end
-
   def self.new_from_db(row)
-    self.new(row[1], row[2], row[0])
+    new_student = self.new()
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
