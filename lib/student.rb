ee3fca2484508778696c5ee9137a88953db85510
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-03-22 21:14:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -69,9 +69,8 @@ class Student
       SELECT * FROM students WHERE grade = 10 LIMIT 1
     SQL
 
-    DB[:conn].execute(sql).map {|row|
-      self.new_from_db(row)
-    }
+    self.new_from_db(DB[:conn].execute(sql)[0])
+    
   end
 
   def self.first_X_students(x)
