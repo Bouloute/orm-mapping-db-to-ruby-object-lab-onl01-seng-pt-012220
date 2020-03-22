ff74090ee94a50872dbf81c250a64b9c67fc9770
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-03-22 21:12:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,14 +65,14 @@ class Student
   end
 
   def self.first_student_in_grade_10
-      sql = <<-SQL
-        SELECT * FROM students LIMIT 1
-      SQL
+    sql = <<-SQL
+      SELECT * FROM students LIMIT 1
+    SQL
 
-      DB[:conn].execute(sql).map {|row|
-        self.new_from_db(row)
-      }
-    end
+    DB[:conn].execute(sql).map {|row|
+      self.new_from_db(row)
+    }
+  end
 
   def self.first_X_students(x)
     sql = <<-SQL
